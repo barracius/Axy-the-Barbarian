b72b69fb72622f5cb4dfb61a91088b77f1c4703f
barracius <rjgonzalez6@gmail.com>
2020-10-09 18:53:01 -0300

    movimiento sin bonus

--- Assets/Scripts/Axy.cs
+++ Assets/Scripts/Axy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Axy : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed = 5;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,35 @@ public class Axy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        inputListener();
+    }
+
+    void inputListener()
     {
         
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {    
+            Debug.Log("arriba");
+            transform.Translate(movementSpeed * Time.deltaTime * Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("izq");
+            transform.Translate(movementSpeed * Time.deltaTime * Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("der");
+            transform.Translate(movementSpeed * Time.deltaTime * Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("abajo");
+            transform.Translate(movementSpeed * Time.deltaTime * Vector3.down);
+        }
     }
+    
+    
 }
