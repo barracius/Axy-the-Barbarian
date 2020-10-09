d02e3162129d35b21b46bd45e39ead23aeec342e
erriveros <erriveros@miuandes.cl>
2020-10-09 18:46:22 -0300

    drunke projectile

--- Assets/Scripts/DrunkSkeleton.cs
+++ Assets/Scripts/DrunkSkeleton.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class DrunkSkeleton : MonoBehaviour
 {
+    public int projectileCoolDown;
+
+    public GameObject projectile;
+
+    private int projectileDistance = 3;
+    private float timeStamp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shoot();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timeStamp <= Time.time)
+        {
+            Shoot();
+        }
+        
+    }
+    void Shoot()
+    {
+        timeStamp = Time.time + projectileCoolDown;
+       
+        int x = Random.Range(-3, 4);
+        int y = Random.Range(-3, 4);
+        while (x==0 && y==0)
+        {
+            x = Random.Range(-3, 4);
+            y = Random.Range(-3, 4);
+        }
+        projectile.transform.position = gameObject.transform.position;
+        projectile.transform.position += new Vector3(x,y,0);
         
     }
 }
