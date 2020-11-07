6962d8134631543db918059452f7842dfe2e9a86
barracius <rjgonzalez6@gmail.com>
2020-11-07 18:15:54 -0300

    Part 10 Done

---
+++ Assets/Scripts/Rat Scripts/RatPhysicsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rat_Scripts;
+using UnityEngine;
+
+public class RatPhysicsController : MonoBehaviour
+{
+    [SerializeField] private RatMainController mainController;
+    [SerializeField] private float fleeingMovementSpeed = 5;
+    [SerializeField] private float wanderingMovementSpeed = 2;
+    private int _randomTemp;
+    
+    void FixedUpdate()
+    {
+        if (mainController.fleeing)
+        {
+            Flee();
+        }
+        else
+        {
+            Wander();
+        }
+    }
+
+    private void Flee()
+    {
+        if (mainController.axyPosition.x < transform.position.x)
+        {
+            transform.Translate(fleeingMovementSpeed * Time.deltaTime * Vector2.right);
+        }
+        else
+        {
+            transform.Translate(fleeingMovementSpeed * Time.deltaTime * Vector2.left);
+        }
+
+        if (mainController.axyPosition.y < transform.position.y)
+        {
+            
+            transform.Translate(fleeingMovementSpeed * Time.deltaTime * Vector2.up);
+        }
+        else 
+        {
+           transform.Translate(fleeingMovementSpeed * Time.deltaTime * Vector2.down); 
+        }
+    }
+    
+    private void Wander()
+    {
+        _randomTemp = Random.Range(0, 3);
+        switch (_randomTemp)
+        {
+            case 0:
+                transform.Translate(wanderingMovementSpeed * Time.deltaTime * Vector2.left);
+                break;
+            case 1:
+                transform.Translate(wanderingMovementSpeed * Time.deltaTime * Vector2.right);
+                break;
+            case 2:
+                transform.Translate(wanderingMovementSpeed * Time.deltaTime * Vector2.up);
+                break;
+            default:
+                transform.Translate(wanderingMovementSpeed * Time.deltaTime * Vector2.down); 
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Rat Scripts/RatTriggerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rat_Scripts;
+using UnityEngine;
+
+public class RatTriggerController : MonoBehaviour
+{
+    [SerializeField] private RatMainController mainController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mainController.fleeing = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mainController.axyPosition = other.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mainController.fleeing = false;
+        }
+    }
+}
